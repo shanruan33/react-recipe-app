6c57b58a670003f1157388edd47c14985590f921
Shan <shanruan333@gmail.com>
2020-02-10 11:04:08 -0500

    react-recipe-search-application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const APP_ID = process.env.REACT_APP_API_ID;
+  const APP_KEY = process.env.REACT_APP_API_KEY;
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('passion fruit');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => { 
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" key="app">
+      <form className="search-form" onSubmit={getSearch}>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <div className="recipes" key="key1">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={Math.round(recipe.recipe.calories)}
+            ingredients={recipe.recipe.ingredients.map(ingredient => (
+              <li key={ingredient.index}>{ingredient.text}</li>
+            ))}
+            image={recipe.recipe.image}
+            // time={recipe.recipe.totalTime}
+            diet={recipe.recipe.dietLabels[0]}
+            link={recipe.recipe.url}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
+const Recipe = ({ title, calories, ingredients, image, time, diet,link }) => {
+  return (
+    <div className="recipe-card">
+      <h1>{title}</h1>
+      <div className="recipe-content">
+        <img src={image} alt='' />
+        <div className="notes">
+          <p>{diet}</p>
+          <p>{calories} calories</p>
+          {/* <p>{time} minutes</p> */}
+          <a href={link}>Instruction</a>          
+        </div>
+        <ol className="ingrediant">
+          {ingredients}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
 
 export default App;
